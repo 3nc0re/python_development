77f02d643f5bc3c59bc3c0a034853956ee18e6bc
3nc0re <22oleg99@gmail.com>
2023-04-18 17:47:51 +0300

    Uploaded homework for lesson 8

--- lesson8/lesson_8_homework.py
+++ lesson8/lesson_8_homework.py
@@ -1,3 +1,5 @@
+# Task #1
+
 from module import greeting
 
 def main():
@@ -6,3 +8,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# Task 3
+def mymod():
+    def count_lines(name):
+        with open(name, 'r') as file:
+            return len(file.readlines())
+
+    def count_chars(name):
+        with open(name, 'r') as file:
+            return len(file.read())
+
+    def test(name):
+        lines = count_lines(name)
+        chars = count_chars(name)
+        print("Lines:", lines)
+        print("Chars:", chars)
